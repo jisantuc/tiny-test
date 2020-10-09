9941f28b7759ead0d90edd308bc73bd5ead04745
James Santucci <james.santucci@gmail.com>
2020-10-09 16:36:30 -0600

    add some propTestN functions

--- src/Arbitrary.hs
+++
@@ -1,4 +0,0 @@
-module Arbitrary (Arbitrary (..)) where
-
-class Arbitrary a where
-  sample :: IO [a]
--- src/PropTest.hs
+++ src/PropTest.hs
@@ -1,15 +1,28 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+module PropTest (prop, prop2, prop3) where
 
-module PropTest (prop) where
-
-import Arbitrary (Arbitrary (..))
 import Data.Foldable (fold)
 import Result (Result)
 
-prop :: forall a. Arbitrary a => (a -> Result) -> IO Result
+class Arbitrary a where
+  sample :: IO [a]
+
+prop :: Arbitrary a => (a -> Result) -> IO Result
 prop f =
   do
     as <- sample
     pure . fold $ f <$> as
+
+prop2 :: (Arbitrary a, Arbitrary b) => (a -> b -> Result) -> IO Result
+prop2 f =
+  do
+    as <- sample
+    bs <- sample
+    pure . fold $ zipWith f as bs
+
+prop3 :: (Arbitrary a, Arbitrary b, Arbitrary c) => (a -> b -> c -> Result) -> IO Result
+prop3 f =
+  do
+    as <- sample
+    bs <- sample
+    cs <- sample
+    pure . fold $ zipWith3 f as bs cs
